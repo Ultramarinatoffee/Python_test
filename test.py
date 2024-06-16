3c1367291b1d858ef13bdf0efa4ba4eced787892
Ultramarinatoffee <marina.polechuk@gmail.com>
2024-06-16 15:04:57 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,41 @@
+# print("Hello, Python in VSCode!")
+# print("Salut, ma petite butterschotch")
+# mon_message = "Salut, ma petite toffee"
+# print(mon_message)
+# a = 1
+# b = 2
+# print(a)
+# print(b)
+# print(a+b)
+# print (a<b)  
+# var = 25
+# print(var)
+# var = var + 1
+# print (var)
+# a = 102
+# while a < 5:
+#     a = a +1
+
+# for cptr in range(3):
+#     print(cptr)
+# nombreMessage = 3
+# for i in range(nombreMessage):
+#     print(mon_message)
+    
+# texte_saisi = input("Tapez n'importe quoi et appuyez sur Entrée : ")
+# print("Vous avez tapé :", texte_saisi)
+
+# nbr_joueur = input("Propose un nombre : ")
+# print("Vous avez donné le nombre: ", nbr_joueur)
+
+a = 1
+print(a == 1)
+a = 2 
+print(a == 1)
+
+print("Hello, world")
+
+resultat = (100 + 81) * 11
+print(resultat)
+print("J'apprends python")
+print(17+35*2)
